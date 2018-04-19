1d6143c92d82e7958d795500c720b76b04242c25
sehenry <sarahelhe@gmail.com>
2018-04-18 19:10:36 -0500

    Worker and Customer controller added. Views created for Creat and Details methods in the Customer class.

--- TrashCollector/Controllers/CustomerController.cs
+++ TrashCollector/Controllers/CustomerController.cs
@@ -2,10 +2,90 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
-    public class CustomerController
+    public class CustomerController : Controller
     {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Customer/Details
+        public ActionResult Details()
+        {
+            var customer = new Customer {firstName = "Sarah", middleInitial = "E", lastName = "Henry", emailAddress = "sarahelhe@gmail.com", password = "password"};
+            return View(customer);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -9,5 +9,47 @@ namespace TrashCollector.Models
     {
         public string emailAddress{ get; set; }
         public string password { get; set; }
+        public string firstName { get; set; }
+        public string middleInitial { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public int id { get; set; }
+
+
+        public void Register()
+        {
+
+        }
+
+        public void Login()
+        {
+
+        }
+        public void SetPickUpDay()
+        {
+
+        }
+
+        public void EditPickUpDay()
+        {
+
+        }
+        public void ChooseFrequency()
+        {
+
+        }
+        public void PausePickup()
+        {
+
+        }
+        public void ViewTrashSchedule()
+        {
+
+        }
+        public void ViewPayments()
+        {
+
+        }
+
     }
 }
